b0d4e16f4514bb20e49bfc65e8880abc920327cf
howger <360000521@qq.com>
2019-04-02 11:42:35 +0800

    commit LichCommentWeb

---
+++ demo0401/build/precache-manifest.a28482af51bf43f1f334c1113b447a27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "85a53e04677365adabce",
+    "url": "./static/js/main.021aa60d.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "./static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "85a53e04677365adabce",
+    "url": "./static/css/main.358b3f94.chunk.css"
+  },
+  {
+    "revision": "49e106966c426b9e89c62bb1e9820123",
+    "url": "./index.html"
+  }
+];
